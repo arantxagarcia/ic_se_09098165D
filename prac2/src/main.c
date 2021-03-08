3542a3bd25e6b293db3ef70b76285d21f4fadd80
arantxagarcia <arantxa.garcia@edu.uah,es>
2021-03-08 09:49:01 +0100

    uplated

---
+++ prac2/src/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+
+ *
+ *  Created on: 06/03/2021
+ *      Author: arantxa
+ */
+#include "leon3_uart.h"
+
+int main(){
+	leon3_putchar('P');
+	leon3_putchar('2');
+	leon3_putchar('\n');
+	return 0;
+}
+
